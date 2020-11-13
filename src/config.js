2700b62e67988ed698f634915ccce1bfb1bb2392
Aeri Jung <rjungdesign@gmail.com>
2020-11-12 22:46:58 -0500

    create end point file

--- src/config.js
+++ src/config.js
@@ -1,13 +1,8 @@
+const endPoints = require("./lib/endPoints");
+
 module.exports = {
   aws: {
     auth: {},
-    endPoint: [
-      {
-        name: "MyCustomLambda",
-        endpoint: "https://lambda.us-east-1.amazonaws.com/2015-03-31/functions/yourFuncName/invocations",
-        service: "lambda",
-        region: "us-east-1"
-      }
-    ]
+    endPoints
   }
 };
