3d8ab3dde565b5cff42d2ca2e00fbca6b570abfb
Aeri Jung <rjungdesign@gmail.com>
2020-11-12 21:41:07 -0500

    test

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
-Amplify.configure(awsExports);
+// Amplify.configure(awsExports);
 
 ReactDOM.render(
   <React.StrictMode>
