ac0be26eae2d061b3841570b1230e8b375c848c1
Aeri Jung <rjungdesign@gmail.com>
2020-11-16 21:46:05 -0500

    add api but not working

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,18 @@
 import Amplify, { API } from "aws-amplify";
-import axios from "axios";
+import { aws } from "./config";
 import "./App.css";
 
+console.log(`${aws.api}/Stage/hello`);
+
 function App() {
-  const test = axios;
-  return <div className="App"></div>;
+  API.get("study-react-amplify", `${aws.api}/Stage/hello`, {
+    headers: {},
+    response: true
+  })
+    .then(response => console.log(response))
+    .catch(error => console.log(error));
+
+  return <div className="App">hello world </div>;
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -1,8 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import Amplify, { API } from "aws-amplify";
+
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import awsConfig from "./aws-exports";
+import { aws } from "./config";
 
 ReactDOM.render(
   <React.StrictMode>
@@ -11,7 +14,19 @@ ReactDOM.render(
   document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+Amplify.configure({
+  API: {
+    endpoints: [
+      {
+        name: "study-react-amplify",
+        endpoint: aws.api,
+        service: "lambda",
+        region: "us-east-2"
+      }
+    ]
+  }
+});
+
+API.configure();
+
+console.log(API);
